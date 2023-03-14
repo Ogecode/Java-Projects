f0f16a546c2dce81972a450051d0a268c3efc7f4
Ogecode <ogechimaryogbonnaya@gmail.com>
2023-03-14 11:48:19 +0100

    Initial version of Demo1 generated by generator-jhipster@7.9.3

---
+++ Demo1/src/main/java/com/ogbonnaya/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.ogbonnaya.repository;
+
+import com.ogbonnaya.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
